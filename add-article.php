3a09e65623f26cf15695ef6b6a123a79a702e361
hamza202 <k8668975@gmail.com>
2018-09-09 13:06:57 +0300

    First commit

---
+++ add-article.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content=""/>
+    <meta name="description" content="Webster - Responsive Multi-purpose HTML5 Template"/>
+    <meta name="author" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>تجربتي | تفاصيل التجربة</title>
+    <?php include 'stylesheet.php' ?>
+</head>
+
+<body>
+<div class="wrapper">
+
+    <!--=================================
+     preloader -->
+
+    <div id="pre-loader">
+        <img src="images/pre-loader/loader-16.gif" alt="">
+    </div>
+
+    <!--=================================
+     preloader -->
+
+    <!--=================================
+     header -->
+
+    <?php include "header.php"; ?>
+
+    <!--=================================
+     header -->
+
+    <!--=================================
+     banner -->
+
+    <section class="page-title bg-overlay-black-60 parallax" data-jarallax='{"speed": 0.6}'
+             style="background-image: url(images/bg1.jpg); padding-top: 190px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-title-name">
+                        <h1>مقالات التجارب</h1>
+                        <p>حدثنا عن تجاربك في الحياه </p>
+                    </div>
+                    <ul class="page-breadcrumb">
+                        <li><a href="index.php"><i class="fa fa-home"></i> الرئيسية</a> <i
+                                    class="fa fa-angle-double-left"></i></li>
+                        <li><span>مقالات التجارب</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--=================================
+     banner -->
+
+    <section class="blog white-bg page-section-ptb">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9">
+
+                    <div class="blog-entry">
+                        sdfsdf
+                    </div>
+                </div>
+                <!-- ================================================ -->
+                <div class="col-lg-3 ">
+                    <div class="team-3 mb-3">
+                        <div class="team team-round full-border">
+                            <div class="team-photo">
+                                <img class="img-fluid mx-auto" src="images/team/01.jpg" alt="">
+                            </div>
+                            <div class="team-description">
+                                <div class="team-info">
+                                    <h5><a href="#">حمزة القحطاني</a></h5>
+                                    <span>خريج جامعة الملك سعود تخصص علم نفس, عام 2014 ,مواليد السعودية جدة 1990 . </span>
+                                </div>
+                                <div class="team-contact">
+                                    <ul class="list-inline info-box">
+                                        <li class="list-inline-item position-relative">
+                                            <a href="#">
+                                                <span>10</span>
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item position-relative">
+                                            <a href="#">
+                                                <span>5</span>
+                                                <i class="fa  fa-heart"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div class="social-icons social-border rounded color-hover clearfix">
+                                        <ul>
+                                            <li class="social-facebook"><a href="#"><i class="fa fa-facebook"></i></a>
+                                            </li>
+                                            <li class="social-twitter"><a href="#"><i class="fa fa-twitter"></i></a>
+                                            </li>
+                                            <li class="social-instagram"><a href="#"><i class="fa fa-instagram"></i></a>
+                                            </li>
+                                            <li class="social-linkedin"><a href="#"><i class="fa fa-linkedin"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <!-- ================================= -->
+            </div>
+        </div>
+    </section>
+
+    <!--=================================
+     footer -->
+
+    <?php include "footer.php" ?>
+
+    <!--=================================
+     footer -->
+
+</div>
+
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>أعلى</span></a></div>
+
+<?php include "javascript.php" ?>
+
+
+</body>
+
+</html>
